9f87ecb083cebe21d411865ef6d8e50e83c69695
datzlauren <ldatz@seas.upenn.edu>
2015-11-28 19:07:01 -0500

    drop pins on map view

--- Identifind/SolveViewController.m
+++ Identifind/SolveViewController.m
@@ -7,8 +7,13 @@
 //
 
 #import "SolveViewController.h"
+#import <MapKit/MapKit.h>
+#import <CoreLocation/CoreLocation.h>
+
 
 @interface SolveViewController ()
+
+@property (weak, nonatomic) IBOutlet MKMapView *mapView;
 
 @end
 
@@ -17,11 +22,40 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    UILongPressGestureRecognizer *gestureRec = [[UILongPressGestureRecognizer alloc]
+                                          initWithTarget:self action:@selector(handleLongPress:)];
+    gestureRec.minimumPressDuration = 0.5;
+    [self.mapView addGestureRecognizer:gestureRec];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)handleLongPress:(UIGestureRecognizer *)gestureRec
+{
+    if (gestureRec.state == UIGestureRecognizerStateBegan) {
+     
+        if (_mapView.annotations.count == 1) {
+            [_mapView removeAnnotations:_mapView.annotations];
+        }
+        CGPoint pointTouched = [gestureRec locationInView:self.mapView];
+        CLLocationCoordinate2D touchedCoord =
+        [self.mapView convertPoint:pointTouched toCoordinateFromView:self.mapView];
+    
+        MKPointAnnotation *pin = [[MKPointAnnotation alloc] init];
+        pin.coordinate = touchedCoord;
+        [self.mapView addAnnotation:pin];
+    
+    } else {
+        return;
+    }
+}
+
+
+
+- (IBAction)submitButtonPressed:(id)sender {
 }
 
 /*
